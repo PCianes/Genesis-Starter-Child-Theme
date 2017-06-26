1c61fa43a963140614f63ed2c0f9648cf8eeffdf
PCianes <cianespablo@gmail.com>
2017-06-26 13:14:30 +0200

    Slicing Up Genesis Sample Functions.php

--- Genesis-sample-functions.php
+++ Genesis-sample-functions.php
@@ -10,6 +10,10 @@
  * @link    http://www.studiopress.com/
  */
 
+/************
+* INIT
+************/
+
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
@@ -30,6 +34,10 @@ define( 'CHILD_THEME_NAME', 'Genesis Sample' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.2.4' );
 
+/************
+* ASSETS
+************/
+
 //* Enqueue Scripts and Styles
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
@@ -46,11 +54,25 @@ function genesis_sample_enqueue_scripts_styles() {
 
 }
 
+/************
+* SETUP
+************/
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'genesis-sample', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'genesis-sample' ) );
+
 //* Add HTML5 markup structure
 add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
 
 //* Add Accessibility support
-add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+add_theme_support( 'genesis-accessibility', array( 
+	'404-page', 
+	'drop-down-menu', 
+	'headings', 
+	'rems', 
+	'search-form', 
+	'skip-links' 
+) );
 
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
@@ -73,11 +95,16 @@ add_theme_support( 'genesis-after-entry-widget-area' );
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
 
+//* Rename primary and secondary navigation menus
+add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
 //* Add Image Sizes
 add_image_size( 'featured-image', 720, 400, TRUE );
 
-//* Rename primary and secondary navigation menus
-add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+/************
+* STRUCTURE/MENU
+************/
 
 //* Reposition the secondary navigation menu
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
@@ -97,6 +124,10 @@ function genesis_sample_secondary_menu_args( $args ) {
 
 }
 
+/************
+* STRUCTURE/POSTS
+************/
+
 //* Modify size of the Gravatar in the author box
 add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
 function genesis_sample_author_box_gravatar( $size ) {
@@ -104,6 +135,11 @@ function genesis_sample_author_box_gravatar( $size ) {
 	return 90;
 
 }
+
+
+/************
+* STRUCTURE/COMMENTS
+************/
 
 //* Modify size of the Gravatar in the entry comments
 add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
